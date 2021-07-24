2e325c70737af7f7a54969dcb683e46b834362ee
Васильев <workgames@mail.ru>
2021-07-24 19:13:46 +0500

    fix: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import cn from 'classnames';
+import '@/assets/scss/app.scss';
 
 export class App extends React.Component {
   render(): JSX.Element {
     let {} = this.props;
-    return <div className={cn('app')}>111111111111111111</div>;
+    return (
+      <div className={cn('app')} style={{ fontSize: '40px' }}>
+        111111111111111111
+      </div>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -48,8 +48,6 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
           },
-          // Creates `style` nodes from JS strings
-          'style-loader',
           // Translates CSS into CommonJS
           'css-loader',
           // Compiles Sass to CSS
